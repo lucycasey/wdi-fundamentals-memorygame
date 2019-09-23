27eb6df1c3648d32c4be81ee81dbd29ac91b485e
Lucy Casey <lucycasey@Lucys-Air.home>
2019-09-23 14:24:04 +0100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+function flipCard (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch ();
+	}
+};
+flipCard (0);
+flipCard (2);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
- } if (cardsInPlay[0] === cardsInPlay[1]) {
- 	alert ("You Found a Match!")
- } else {
-    alert("Sorry, Try Again.")
- }
